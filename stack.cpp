34bb4fcb78ffde4dc87fac296a913eea6de940ab
Hani-hue <isnahabibah8729@gmail.com>
2025-06-05 11:09:11 +0700

    case 1 pada switch

--- stack.cpp
+++ stack.cpp
@@ -92,5 +92,10 @@ int main()
 
     switch (choice)
     {
-        }
+    case 1:
+        cout << "Enter the value to push: ";
+        cin >> value;
+        stack.push(value);
+        break;
+    }
 }
