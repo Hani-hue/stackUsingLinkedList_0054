43b431046df032df8ff925e143db60daeb19a53a
Hani-hue <isnahabibah8729@gmail.com>
2025-06-05 11:05:47 +0700

    membuat output dalam fungsi while

--- stack.cpp
+++ stack.cpp
@@ -82,5 +82,11 @@ int main()
 
     while (choice != 4)
     {
-        }
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+    }
 }
