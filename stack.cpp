67f3d94eb41cded4fa0b12f5bad042d18c4d4bf0
Hani-hue <isnahabibah8729@gmail.com>
2025-06-05 10:51:06 +0700

    kembalikan nilai ke top node

--- stack.cpp
+++ stack.cpp
@@ -53,7 +53,7 @@ public:
         cout << "popped value: " << top->data << endl;
         top = top->next; // update the top pionter to the next node
     }
-
+    // peek/top operation: retrieve the value of the topmost element without removing it
     void peek()
     {
         if (isEmpty())
@@ -61,5 +61,14 @@ public:
             cout << "List is empty." << endl;
         }
         else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
     }
 };
